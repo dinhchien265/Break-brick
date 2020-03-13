39c6f183d39970bc407e29ef9141a1b0f5c4e517
dinhchien265 <dinhchien26599@gmail.com>
2020-03-13 22:28:53 +0700

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float Force = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.parent = null;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(Force, 0, Force);
+        }
+    }
+}
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x=Mathf.Clamp(temp.x + Input.GetAxis("Horizontal") * speed*Time.deltaTime,-4.5f,4.5f);
+        transform.position = temp;
+    }
+}
